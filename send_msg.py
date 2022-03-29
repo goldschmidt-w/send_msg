88395ec7730ec757756814868b744992dcb0df25
goldschmidt-w <vonnov.w@gmail.com>
2022-03-29 19:22:37 +0800

    20220329_001

---
+++ send_msg.py
@@ -0,0 +1,14 @@
+from twilio.rest import Client
+
+account_sid = ''
+
+
+auth_token = ''
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+	to = "+",
+	from_ = "",
+	body = "",
+	)
